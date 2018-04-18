eb0207f0dd0caa8d4a669a5f609d214592da40dd
wei-jun <weijun.zeng@mail.utoronto.ca>
2018-04-17 23:54:29 -0400

    Adds ListView with custom colors

--- app/src/main/java/com/apress/gerber/reminders/RemindersActivity.java
+++ app/src/main/java/com/apress/gerber/reminders/RemindersActivity.java
@@ -2,6 +2,8 @@ package com.apress.gerber.reminders;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class RemindersActivity extends AppCompatActivity {
 
@@ -9,5 +11,22 @@ public class RemindersActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reminders);
+
+        ListView mListView = (ListView)findViewById(R.id.reminders_list_view);
+        // The arrayAdapter is the controller in our
+        // model-view-controller relationship. (controller)
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(
+                // context
+                this,
+                // layout (viewI
+                R.layout.reminders_row,
+                // row (view)
+                R.id.row_text,
+                // data (model) with bogus data to test our listview
+                new String[]{"first record", "second record", "third record"}
+        );
+
+        mListView.setAdapter(arrayAdapter);
     }
+    // Remainder of the class listing amitted for brevity
 }
